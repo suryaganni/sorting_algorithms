83b2981c15f052709327a31840a2ecf1ca9932d8
suryag <suryag@surya-g.(none)>
2013-12-06 12:06:36 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SortingAlgorithms::Application.config.secret_token = 'bebd135a029c3853e5f8b8aefbc697f15f5bb801637afd681cd8570ec007221ee795ab0c9b7bbd7b648115af8735134a03589638c2915e0d58aa91b11a5e310c'
